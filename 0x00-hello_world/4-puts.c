c7021381273d26d6e7ffb11203e8d620eb6cec71
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-14 08:54:09 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print a text and
+*
+*Return 0(success)
+*/
+
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
