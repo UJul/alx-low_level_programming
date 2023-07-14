545cd0c2d3647ae11deb9c7c78c12729236e0c14
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-14 10:16:19 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 /**
-* main - Entry point
-* Description: Programming is like building a multilingual puzzle
-* Retturn: 0
-*/
+ * main - prints a text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
