5c6fd659cce522199b145a134bb1f8e4853571ab
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-19 20:52:59 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main -  The Program prints 10 times the alphabet.
+ * main - The Program prints 10 times the alphabet.
  * In lowercase, followed by a new line.
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
 	int a;
 	char b;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 'a'; b <= 'z'; b++)
