7268dd3d57531ff8a282c5f16566587ee9e2aea3
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-19 21:30:57 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Program that writes a function that checks for lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return 1;
+    }
+    return 0;
+}
