92a0b546593bd200964e9462e69d33c0932b253e
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-19 17:40:47 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-  /**
-  *main - entry point of the program 
-  *
-  *Return: Always 0
-  */ 
+/**
+*main - entry point of the program 
+*
+*Return: Always 0
+*/ 
 int main(void)
 {
 	_putchar('_');
