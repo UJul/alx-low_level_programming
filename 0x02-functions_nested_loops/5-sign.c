a58783ab7049c766821d0e82889fb5e72be18bbf
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-20 09:56:26 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *print_sign - Print text.
+  *
+  *Return : Always 1
+ */
+int print_sign(int n)
+                {
+                    if (n > 0)
+                        return 1;
+}
