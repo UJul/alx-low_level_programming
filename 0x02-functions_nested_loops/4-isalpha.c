6e7d99556afb7dc9e55874709089743b5c9b0e07
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-20 08:29:01 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,14 @@
+#include "main.h"
 
+/**
+  *_isalpha - Prints text.
+  *@c : Print @.
+  *Return : 0.
+ */
+
+int _islower(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return 1;
+    }
+    return 0;
+}
