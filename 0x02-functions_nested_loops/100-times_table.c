68187d10b878814fb751b130f9fdca2adc577578
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-20 13:35:35 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,39 @@
 #include "main.h"
 
 /**
-  *print_times_table -print text
-  *
+  *print_times_table - print text
+  *@n: print @n
   *Return: Always 0
  */
 void print_times_table(int n)
 {
-if (n <= 0 || n >= 15)
-return;
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+	for (n = 0; num <= n; num++)
+	{
+	_putchar("0");
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(",");
+	_putchar(" ");
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(" ");
+	if (prod <= 9)
+	_putchar(" ");
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
 }
