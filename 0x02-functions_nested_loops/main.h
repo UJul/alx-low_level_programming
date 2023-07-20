3dda850779ca7ff6d67cb1a4370662b7be5167bd
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-20 16:01:12 +0100

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int add(int, int);
+void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
 void print_times_table(int n);
