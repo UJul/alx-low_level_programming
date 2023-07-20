03093218bc62a8a8f528026233a223185d3a6c43
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-20 14:41:48 +0100

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,10 +7,7 @@
  */
 int add(int, int);
 {
-int number1, number2, sum;
-printf("Enter two integers: ");
-scanf("%d %d", &number1, &number2);
-sum = number1 + number2;
-printf("%d + %d = %d", number1, number2, sum);
+int a, int b;
+return (a + b);
 return (0);
 }
