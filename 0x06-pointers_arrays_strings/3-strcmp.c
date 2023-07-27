55e481f677ec91b0f12b839634143185f6ebbcfc
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-07-27 07:02:40 +0100

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,23 +6,17 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1.
+ * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of the first unmatched characters.
  */
-int _strcmp(char *s1, char *s2) {
-    int i = 0;
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] < s2[i])
-            return -1;
-        else if (s1[i] > s2[i])
-            return 1;
-        i++;
-    }
-
-    if (s1[i] == '\0' && s2[i] == '\0')
-        return 0;
-    else if (s1[i] == '\0')
-        return -1;
-    
-    return 1;
+	return (*s1 - *s2);
 }
