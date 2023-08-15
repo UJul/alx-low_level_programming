77e8429aaed1887b974b6f9924f6850abdeedf3a
okereke obianuju chiamaka <okereke.chiamaka@yahoo.com>
2023-08-15 23:32:27 +0100

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,13 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 /*
- * File: 3-function_like_macro.
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
+ * File: 0-object_like_macro.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
  */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define SIZE 1024
 
 #endif
